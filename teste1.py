9433dfab11da00580da0c41654f00d271a8f0aef
joaoreiis <joao_reiis@hotmail.com>
2018-12-23 20:14:20 -0200

    Add files via upload

---
+++ teste1.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+
+	_, frame = cap.read()
+
+	cv2.imshow("frame", frame)
+
+	key = cv2.waitKey(1)
+
+	if key == 27:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
